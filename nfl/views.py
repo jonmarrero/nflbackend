bb10cb9fb122dd7109b5feb2085401dba365c736
Jonathan Marrero <Jonathanmarrero24@gmail.com>
2023-02-27 17:47:07 -0500

    views.py

--- nfl/views.py
+++ nfl/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Nfl
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import NflSerializer
 # Create your views here.
+
+class NflViewSet(viewsets.ModelViewSet):
+    queryset = Nfl.objects.all()
+    serializer_class = NflSerializer
+    permission_classes = [permissions.AllowAny]
