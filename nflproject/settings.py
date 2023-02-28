696256ae6e0dbcd9623a158f20eecdf2e5727b87
Jonathan Marrero <Jonathanmarrero24@gmail.com>
2023-02-27 20:12:39 -0500

    migrations

--- nflproject/settings.py
+++ nflproject/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "nfl.apps.nflprojectConfig", 
+    "nfls.apps.nflprojectConfig", 
     "rest_framework",
     "corsheaders"
 ]
